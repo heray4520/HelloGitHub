0617f50ef854943e6f9f3bff01198ea384c5c0fd
heray <heray@bjpowernode.com>
2020-01-08 11:50:06 +0800

    初始提交,添加了id

---
+++ src/main/java/com/bjpowernode/git/User.java
@@ -0,0 +1,15 @@
+package com.bjpowernode.git;
+
+/**
+ * ClassName:User
+ * Package:com.bjpowernode.git
+ * Description:
+ *
+ * @date:2020/1/8 11:46
+ * @author:邓浩东
+ */
+public class User {
+
+    private String id;
+
+}
